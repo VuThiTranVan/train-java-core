0a7b96c8f605f352f530f5001a40065b53980e87
vuthitranvan <vu.thi.tran.van@framgia.com>
2017-07-04 10:28:43 +0700

    example subclass and supper class : extends

---
+++ src/framgia/co/train/java/lesson2/Car.java
@@ -0,0 +1,33 @@
+package framgia.co.train.java.lesson2;
+
+public class Car extends Vehicle {
+	private String producttor;
+
+	public Car(String producttor) {
+		super();
+		this.producttor = producttor;
+	}
+
+	public Car(String name, String color, float speed, String producttor) {
+		super(name, color, speed);
+		this.producttor = producttor;
+	}
+
+	public Car() {
+	}
+
+	@Override
+	public String display() {
+		return super.display() + "\tProducttor: " + producttor;
+
+	}
+
+	public String getProducttor() {
+		return producttor;
+	}
+
+	public void setProducttor(String producttor) {
+		this.producttor = producttor;
+	}
+
+}
